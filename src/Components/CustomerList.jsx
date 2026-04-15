e2913c01921908b61fc08eda12683bc428cf5c23
SheksDev <sekemiariyibi@gmail.com>
2026-04-15 08:52:49 +0100

    Modified the Edit and Delete Buttons

--- src/Components/CustomerList.jsx
+++ src/Components/CustomerList.jsx
@@ -7,9 +7,13 @@ class CustomerList extends Component {
     return (
       <div className='w-full'>
         <h1 className='text-2xl font-bold mb-8'>Customer List</h1>
+
         {/* Customer Cards Container */}
+
         <div className='grid grid-flow-row grid-cols-2 gap-4'>
-          {/* Cards Container */}
+
+          {/* Card Container */}
+
           <div className='bg-[#FFFFFF] rounded-2xl p-6 shadow-xl'>
             <div className='flex items-center justify-between mb-4'>
               <div className='bg-[#DBE1FF] text-[#346EF6] font-bold w-12 h-12 rounded-full flex items-center justify-center text-lg'>AJ</div>
@@ -35,16 +39,18 @@ class CustomerList extends Component {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-3.5 text-[#C9510D]">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
                 </svg>
-                <p className='text-[#C9510D] text-xs font-bold ml-2'>Edit</p>
+                <button className='text-[#C9510D] text-xs font-bold ml-2 cursor-pointer'>Edit</button>
               </div>
               <div className='flex items-center justify-start cursor-pointer'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-3.5 text-[#581E00]">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                 </svg>
-                <p className='text-[#581E00] text-xs font-bold ml-2'>Delete</p>
+                <button className='text-[#581E00] text-xs font-bold ml-2 cursor-pointer'>Delete</button>
               </div>
             </div>
           </div>
+
+          {/* Card Container */}
 
           <div className='bg-[#FFFFFF] rounded-xl p-6 shadow-2xl'>
             <div className='flex items-center justify-between mb-4'>
@@ -71,16 +77,18 @@ class CustomerList extends Component {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-3.5 text-[#C9510D]">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
                 </svg>
-                <p className='text-[#C9510D] text-xs font-bold ml-2'>Edit</p>
+                <button className='text-[#C9510D] text-xs font-bold ml-2 cursor-pointer'>Edit</button>
               </div>
               <div className='flex items-center justify-start cursor-pointer'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-3.5 text-[#581E00]">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                 </svg>
-                <p className='text-[#581E00] text-xs font-bold ml-2'>Delete</p>
+                <button className='text-[#581E00] text-xs font-bold ml-2 cursor-pointer'>Delete</button>
               </div>
             </div>
           </div>
+
+          {/* Card Container */}
 
           <div className='bg-[#FFFFFF] rounded-xl p-6 shadow-2xl'>
             <div className='flex items-center justify-between mb-4'>
@@ -107,16 +115,18 @@ class CustomerList extends Component {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-3.5 text-[#C9510D]">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
                 </svg>
-                <p className='text-[#C9510D] text-xs font-bold ml-2'>Edit</p>
+                <button className='text-[#C9510D] text-xs font-bold ml-2 cursor-pointer'>Edit</button>
               </div>
               <div className='flex items-center justify-start cursor-pointer'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-3.5 text-[#581E00]">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                 </svg>
-                <p className='text-[#581E00] text-xs font-bold ml-2'>Delete</p>
+                <button className='text-[#581E00] text-xs font-bold ml-2 cursor-pointer'>Delete</button>
               </div>
             </div>
           </div>
+
+          {/* Card Container */}
 
           <div className='bg-[#FFFFFF] rounded-2xl p-6 shadow-xl'>
             <div className='flex items-center justify-between mb-4'>
@@ -143,13 +153,13 @@ class CustomerList extends Component {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-3.5 text-[#C9510D]">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
                 </svg>
-                <p className='text-[#C9510D] text-xs font-bold ml-2'>Edit</p>
+                <button className='text-[#C9510D] text-xs font-bold ml-2 cursor-pointer'>Edit</button>
               </div>
               <div className='flex items-center justify-start cursor-pointer'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-3.5 text-[#581E00]">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                 </svg>
-                <p className='text-[#581E00] text-xs font-bold ml-2'>Delete</p>
+                <button className='text-[#581E00] text-xs font-bold ml-2 cursor-pointer'>Delete</button>
               </div>
             </div>
           </div>
