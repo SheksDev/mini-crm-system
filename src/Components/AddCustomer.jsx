d762ee56d3765dfa987e840a7bbc2b3e5f33df4a
SheksDev <sekemiariyibi@gmail.com>
2026-04-15 10:25:28 +0100

    Handled the Add Customer Form Submission

--- src/Components/AddCustomer.jsx
+++ src/Components/AddCustomer.jsx
@@ -1,27 +1,62 @@
 import React, { Component } from 'react'
 
 class AddCustomer extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      name: "",
+      email: "",
+      phone: ""
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = (e) => {
+
+    const { sendData } = this.props;
+
+    e.preventDefault();
+    console.log(this.state);
+    sendData(this.state);
+  }
+
   render() {
+
     return (
       <div className='bg-[#FFFFFF] rounded-2xl p-8 shadow-xl'>
         <h1 className='text-[#191C1D] text-2xl font-bold mb-2'>Add Customer</h1>
         <p className='text-[#5C5F60] text-sm mb-4'>Register a new lead into the workspace</p>
+
         {/* Add Customer Form */}
-        <form action="submit" className='space-y-5'>
+        <form action="" onSubmit={this.handleSubmit} className='space-y-5'>
+
+          {/* Input Name */}
           <div>
             <label htmlFor="" className='text-sm mb-2 block'>Name</label>
-            <input type="text" placeholder='Full name' className='border rounded-xl border-[#E1E3E4] px-4 py-3 text-sm outline-none w-full'/>
+            <input type="text" name='name' onChange={this.handleChange} placeholder='Full name' className='border rounded-xl border-[#E1E3E4] px-4 py-3 text-sm outline-none w-full'/>
           </div>
+
+          {/* Input Email */}
           <div>
             <label htmlFor="" className='text-sm mb-2 block'>Email</label>
-            <input type="text" placeholder='email@company.com' className='border rounded-xl border-[#E1E3E4] px-4 py-3 text-sm outline-none w-full'/>
+            <input type="text" name='email' onChange={this.handleChange} placeholder='email@company.com' className='border rounded-xl border-[#E1E3E4] px-4 py-3 text-sm outline-none w-full'/>
           </div>
+
+          {/* Input Phone */}
           <div>
             <label htmlFor="" className='text-sm mb-2 block'>Phone</label>
-            <input type="text" placeholder='+1 (555) 000-0000' className='border rounded-xl border-[#E1E3E4] px-4 py-3 text-sm outline-none w-full'/>
+            <input type="text" name='phone' onChange={this.handleChange} placeholder='+1 (555) 000-0000' className='border rounded-xl border-[#E1E3E4] px-4 py-3 text-sm outline-none w-full'/>
           </div>
+
           {/* Add Button */}
-          <button className='w-full py-4 font-bold rounded-xl shadow-lg mt-4 bg-[#346EF6] hover:bg-[#DBE1FF] text-[#FFFFFF] hover:text-[#346EF6] cursor-pointer'>Add Customer</button>
+          <button type='submit' className='w-full py-4 font-bold rounded-xl shadow-lg mt-4 bg-[#346EF6] hover:bg-[#DBE1FF] text-[#FFFFFF] hover:text-[#346EF6] cursor-pointer'>Add Customer</button>
         </form>
       </div>
     )
--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -3,10 +3,25 @@ import AddCustomer from './AddCustomer';
 import CustomerList from './CustomerList';
 
 export class Body extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      formData: {}
+    };
+  }
+
+  updateData = (data) => {
+    this.setState({ formData: data }, () => {
+      console.log(this.state);
+    });
+  };
+
   render() {
     return (
       <div className=' mt-21 p-6 grid grid-cols-[1fr_2fr] gap-8 font-["Inter"] items-start'>
-        <AddCustomer />
+        <AddCustomer sendData={this.updateData} />
         <CustomerList />
       </div>
     )
